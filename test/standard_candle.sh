77ccab50a855c5f841380960cba3688ab43872d2
Danilo Piparo <sha1-e2231637d47316c544de5e218528626d57de224e@cern.ch>
2007-05-21 15:22:05 +0000

    removed profiler:plain process

--- test/standard_candle.sh
+++ test/standard_candle.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 
 echo A standard candle benchmark: QCD with 20GeV < Pt < 30GeV.
-
-echo Getting ready Sim and Digi steps and run the reco without output.
-$CMSSW_BASE/src/Configuration/PyReleaseValidation/test/relvalreport.py 12 3 RECO 15 3_13 0
+$CMSSW_BASE/src/Configuration/PyReleaseValidation/data/cmsDriver.py QCD -n10 -e20_30 -sALL
