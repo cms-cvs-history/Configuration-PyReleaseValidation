d3fb2a6d5cd6e9cacbb966942fed1c2a529e83b2
David Lange <sha1-b4ab71fd0383141ecc8720f477f1ba20f2889d65@cern.ch>
2008-02-11 01:39:34 +0000

    adding back full reco seq and fixing (sort of) cmsDriver_commands.txt

--- data/relval_steps_module.py
+++ data/relval_steps_module.py
@@ -85,11 +85,7 @@ def reco(process):
     '''
     func_id=mod_id+"["+sys._getframe().f_code.co_name+"]"
     
-# conv sequence is broken in 200pre1 candidate, so turn off for now
-# RS sequence should be off by default (if I understand Shahrams instructions
-#
-#    process.reconstruction_step=cms.Path(process.reconstruction_plusRS)
-    process.reconstruction_step=cms.Path(process.reconstruction_woRS_woConv)
+    process.reconstruction_step=cms.Path(process.reconstruction_plusRS)
     if not user_schedule:
         process.schedule.append(process.reconstruction_step)     
 
