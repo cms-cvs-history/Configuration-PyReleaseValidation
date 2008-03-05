085efb452e4e3a3292869c1f2f7eaacb2033043e
Gabriele Benelli <sha1-9f689a57ab1cff5a2566702b3cac6c22c95334fa@cern.ch>
2008-03-05 13:54:56 +0000

    -Improved SimulationG4.py fragment -Fixed a mismatch bug between commands and html output files -Fixed valgrind memcheck issue in cmsRelvalreport.py -Aesthetic change in cmsTiming_parser.py (to fix a Root display issue appeared in CMSSW2_0_0_pre3)
     Committing in .
    
     Modified Files:
     	data/SimulationG4.py scripts/cmsIgProf_Analysis.py
     	scripts/cmsRelvalreport.py scripts/cmsTiming_parser.py
     ----------------------------------------------------------------------

--- data/SimulationG4.py
+++ data/SimulationG4.py
@@ -23,28 +23,42 @@ def customise(process):
     
     #Tweak Message logger to dump G4cout and G4cerr messages in G4msg.log
     #print process.MessageLogger.__dict__
-    process.MessageLogger.destinations=cms.untracked.vstring('warnings'
-                                                             , 'errors'
-                                                             , 'infos'
-                                                             , 'debugs'
-                                                             , 'cout'
-                                                             , 'cerr'
-                                                             , 'G4msg'
+    process.MessageLogger.destinations=cms.untracked.vstring('cout'
+                                                             ,'cerr'
+                                                             ,'G4msg'
                                                              )
     process.MessageLogger.categories=cms.untracked.vstring('FwkJob'
                                                            ,'FwkReport'
                                                            ,'FwkSummary'
                                                            ,'Root_NoDictionary'
+                                                           ,'TimeReport'
+                                                           ,'TimeModule'
+                                                           ,'TimeEvent'
+                                                           ,'MemoryCheck'
+                                                           ,'PhysicsList'
                                                            ,'G4cout'
                                                            ,'G4cerr'
                                                            )
-    process.MessageLogger.cerr = cms.untracked.PSet(
-        noTimeStamps = cms.untracked.bool(True)
-        )
+    #Configuring the G4msg.log output
     process.MessageLogger.G4msg =  cms.untracked.PSet(
         noTimeStamps = cms.untracked.bool(True)
+        #First eliminate unneeded output
         ,threshold = cms.untracked.string('INFO')
         ,INFO = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,FwkReport = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,FwkSummary = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,Root_NoDictionary = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,FwkJob = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,TimeReport = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,TimeModule = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,TimeEvent = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        ,MemoryCheck = cms.untracked.PSet(limit = cms.untracked.int32(0))
+        #TimeModule, TimeEvent, TimeReport are written to LogAsbolute instead of LogInfo with a category
+        #so they cannot be eliminated from any destination (!) unless one uses the summaryOnly option
+        #in the Timing Service... at the price of silencing the output needed for the TimingReport profiling
+        #
+        #Then add the wanted ones:
+        ,PhysicsList = cms.untracked.PSet(limit = cms.untracked.int32(-1))
         ,G4cout = cms.untracked.PSet(limit = cms.untracked.int32(-1))
         ,G4cerr = cms.untracked.PSet(limit = cms.untracked.int32(-1))
         )
