97bccc494acea215f5a6777b4a9245bf1049a0ff
Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch>
2009-10-05 20:13:26 +0000

    fix handling of imports

--- python/ConfigBuilder.py
+++ python/ConfigBuilder.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 
-__version__ = "$Revision: 1.145 $"
+__version__ = "$Revision: 1.147 $"
 __source__ = "$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v $"
 
 import FWCore.ParameterSet.Config as cms
@@ -318,8 +318,8 @@ class ConfigBuilder(object):
 		print defaultOptions.scenarioOptions
 		sys.exit(-1)
 		
-        self.executeAndRemember('Configuration/StandardSequences/Services_cff')
-	self.executeAndRemember('FWCore/MessageService/MessageLogger_cfi')
+        self.loadAndRemember('Configuration/StandardSequences/Services_cff')
+	self.loadAndRemember('FWCore/MessageService/MessageLogger_cfi')
 
 	self.ALCADefaultCFF="Configuration/StandardSequences/AlCaRecoStreams_cff"    
 	self.GENDefaultCFF="Configuration/StandardSequences/Generator_cff"
@@ -775,7 +775,7 @@ class ConfigBuilder(object):
     def build_production_info(self, evt_type, evtnumber):
         """ Add useful info for the production. """
         prod_info=cms.untracked.PSet\
-              (version=cms.untracked.string("$Revision: 1.145 $"),
+              (version=cms.untracked.string("$Revision: 1.147 $"),
                name=cms.untracked.string("PyReleaseValidation"),
                annotation=cms.untracked.string(evt_type+ " nevts:"+str(evtnumber))
               )
