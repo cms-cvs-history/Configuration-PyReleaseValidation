0cb19da653c7722d094d9b1610cdd390becff913
cmsbuild <>
2010-01-19 10:46:27 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- PyReleaseValidation-V01-17-33-01'.
    Sprout from master 2010-01-19 10:46:26 UTC Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch> 'swapped (on request of Andrea Perrotta) fastsim samples: std 5.1, 6.1, 6.2, 6.3 <-> high 23.3, 24.1, 24.4, 24.5'
    Cherrypick from master 2010-01-14 13:40:13 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'add MC specific handling of AlcaRecoStreams':
        python/ConfigBuilder.py

--- python/ConfigBuilder.py
+++ python/ConfigBuilder.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 
-__version__ = "$Revision: 1.158 $"
+__version__ = "$Revision: 1.156 $"
 __source__ = "$Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v $"
 
 import FWCore.ParameterSet.Config as cms
@@ -104,6 +104,10 @@ class ConfigBuilder(object):
     def addCommon(self):
         if 'HARVESTING' in self._options.step:
             self.process.options = cms.untracked.PSet( Rethrow = cms.untracked.vstring('ProductNotFound'),fileMode = cms.untracked.string('FULLMERGE'))
+        else:
+            # use very strict settings
+            from FWCore.Framework.test.cmsExceptionsFatalOption_cff import Rethrow
+            self.process.options = cms.untracked.PSet( Rethrow = Rethrow )
             
     def addMaxEvents(self):
         """Here we decide how many evts will be processed"""
@@ -813,7 +817,7 @@ class ConfigBuilder(object):
     def build_production_info(self, evt_type, evtnumber):
         """ Add useful info for the production. """
         prod_info=cms.untracked.PSet\
-              (version=cms.untracked.string("$Revision: 1.158 $"),
+              (version=cms.untracked.string("$Revision: 1.156 $"),
                name=cms.untracked.string("PyReleaseValidation"),
                annotation=cms.untracked.string(evt_type+ " nevts:"+str(evtnumber))
               )
@@ -855,8 +859,7 @@ class ConfigBuilder(object):
         self.pythonCfgCode += "\nprocess.maxEvents = "+self.process.maxEvents.dumpPython()
 
         # dump the job options
-	if hasattr(self.process,"options"):
-            self.pythonCfgCode += "\nprocess.options = "+self.process.options.dumpPython()
+        self.pythonCfgCode += "\nprocess.options = "+self.process.options.dumpPython()
 
         # dump the input definition
         self.pythonCfgCode += "\n# Input source\n"
