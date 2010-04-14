a3db0ae6006938e4e5336ca6442fe29c88011470
Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch>
2010-04-14 19:32:59 +0000

    added value for com10 for 2010 data reco relval

--- python/autoCond.py
+++ python/autoCond.py
@@ -3,4 +3,5 @@ autoCond = { 'mc'      : 'MC_3XY_V24::All',
              'startup' : 'START3X_V24::All',
              'craft08' : 'CRAFT08_R_V9::All',
              'craft09' : 'CRAFT09_R_V10::All',
+             'com10'   : 'GR_R_35X_V7::All'
              }
