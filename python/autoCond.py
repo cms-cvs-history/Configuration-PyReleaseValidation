3518706db683937ab36e9e7eda32db97384015fb
cmsbuild <>
2010-05-05 12:40:36 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- PyReleaseValidation-V01-18-15-02'.
    Sprout from master 2010-04-20 19:29:49 UTC Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch> 'add harvesting to some (4.1, 4.4. and 10) samples'
    Cherrypick from master 2010-04-27 14:07:43 UTC Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch> 'fix cleanup (do this now in the wfDir); add ulimit -v 4096000 to stop processes from using too much memory':
        scripts/runTheMatrix.py
    Cherrypick from Configuration-PyReleaseValidation-gc-v100505-36X-branch 2010-05-05 12:40:35 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Update GTs for 36X':
        python/autoCond.py

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,8 +1,8 @@
 
 
-autoCond = { 'mc'      : 'MC_37Y_V0::All',
-             'startup' : 'START37_V0::All',
-             'craft08' : 'CRFT8_37R_V0::All',
-             'craft09' : 'CRFT9_37R_V0::All',
-             'com10'   : 'GR_R_37X_V0::All'
+autoCond = { 'mc'      : 'MC_36Y_V6::All',
+             'startup' : 'START36_V6::All',
+             'craft08' : 'CRAFT08_R_V12::All',
+             'craft09' : 'CRFT9_36R_V02::All',
+             'com10'   : 'GR_R_36X_V6::All'
              }
