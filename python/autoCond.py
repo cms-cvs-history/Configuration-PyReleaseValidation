dafcd38974163fa92c3248b691018bcc1d1b366f
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-05-05 12:40:35 +0000

    Update GTs for 36X

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,7 +1,7 @@
 
 
-autoCond = { 'mc'      : 'MC_36Y_V5::All',
-             'startup' : 'START36_V5::All',
+autoCond = { 'mc'      : 'MC_36Y_V6::All',
+             'startup' : 'START36_V6::All',
              'craft08' : 'CRAFT08_R_V12::All',
              'craft09' : 'CRFT9_36R_V02::All',
              'com10'   : 'GR_R_36X_V6::All'
