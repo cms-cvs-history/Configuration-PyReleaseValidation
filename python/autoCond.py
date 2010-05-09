dc36d0f210df75fdb1a238f7b34c6d227d2adc2d
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-05-09 19:35:31 +0000

    Update GTs for 36X (mainly backport of ECAL noise model and related ES objects

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,8 +1,8 @@
 
 
-autoCond = { 'mc'      : 'MC_36Y_V6::All',
-             'startup' : 'START36_V6::All',
+autoCond = { 'mc'      : 'MC_36Y_V7::All',
+             'startup' : 'START36_V7::All',
              'craft08' : 'CRAFT08_R_V12::All',
-             'craft09' : 'CRFT9_36R_V02::All',
-             'com10'   : 'GR_R_36X_V6::All'
+             'craft09' : 'CRFT9_36R_V03::All',
+             'com10'   : 'GR_R_36X_V8::All'
              }
