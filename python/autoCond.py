bd718804ef59022515940981605556ff639db74c
cmsbuild <>
2010-05-19 21:30:42 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- PyReleaseValidation-V01-18-15-08'.
    Sprout from Configuration-PyReleaseValidation-ap-v011815-36x-branch 2010-05-19 21:30:41 UTC Elizabeth Sexton-Kennedy <sha1-3498bc7485b3587b4b87ba552af7f44c37610528@cern.ch> 'Back port the change to the default setting for the geometry to 36X'
    Cherrypick from Configuration-PyReleaseValidation-gc-v100505-36X-branch 2010-05-18 18:42:53 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Update GTs for 36X fixing ES problem':
        python/autoCond.py

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,8 +1,8 @@
 
 
-autoCond = { 'mc'      : 'MC_36Y_V7A::All',
-             'startup' : 'START36_V7::All',
+autoCond = { 'mc'      : 'MC_36Y_V8::All',
+             'startup' : 'START36_V8::All',
              'craft08' : 'CRAFT08_R_V12::All',
              'craft09' : 'CRFT9_36R_V03::All',
-             'com10'   : 'GR_R_36X_V9::All'
+             'com10'   : 'GR_R_36X_V10::All'
              }
