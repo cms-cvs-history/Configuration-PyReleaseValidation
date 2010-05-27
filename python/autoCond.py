2e3d4b253fa0937f7c708b8914f461685a55917c
Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch>
2010-05-27 09:20:01 +0000

    added hltonline as requested by Martin

--- python/autoCond.py
+++ python/autoCond.py
@@ -4,5 +4,6 @@ autoCond = { 'mc'      : 'MC_36Y_V7A::All',
              'startup' : 'START36_V7::All',
              'craft08' : 'CRAFT08_R_V12::All',
              'craft09' : 'CRFT9_36R_V03::All',
-             'com10'   : 'GR_R_36X_V9::All'
+             'com10'   : 'GR_R_36X_V9::All',
+             'hltonline' : 'GR10_H_V6A::All',
              }
