1071da27f5df63e7a7b6cb87cc157dd1b4ddb582
cmsbuild <>
2010-07-29 13:55:00 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- PyReleaseValidation-V01-18-28-04'.
    Sprout from master 2010-05-25 14:09:16 UTC Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch> 'check if dirout is in options before using it'
    Cherrypick from Configuration-PyReleaseValidation-gc-v100729-37X-branch 2010-07-29 13:54:59 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Update GTS for 37X':
        python/autoCond.py

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,8 +1,9 @@
 
 
-autoCond = { 'mc'      : 'MC_37Y_V4::All',
-             'startup' : 'START37_V4::All',
+autoCond = { 'mc'      : 'MC_37Y_V6::All',
+             'startup' : 'START37_V6::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_37R_V4::All',
-             'com10'   : 'GR_R_37X_V4::All'
+             'com10'   : 'GR_R_37X_V6::All',
+             'hltonline' : 'GR_R_37X_V6::All' # same as com10 until H tag available
              }
