65422ea59b172708611ff0ee076301e8d0c20de9
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-07-29 13:54:59 +0000

    Update GTS for 37X

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,9 +1,9 @@
 
 
-autoCond = { 'mc'      : 'MC_37Y_V5::All',
-             'startup' : 'START37_V5::All',
+autoCond = { 'mc'      : 'MC_37Y_V6::All',
+             'startup' : 'START37_V6::All',
              'craft08' : 'CRFT8_37R_V0::All',
              'craft09' : 'CRFT9_37R_V4::All',
-             'com10'   : 'GR_R_37X_V5::All',
-             'hltonline' : 'GR_R_37X_V5::All' # same as com10 until H tag available
+             'com10'   : 'GR_R_37X_V6::All',
+             'hltonline' : 'GR_R_37X_V6::All' # same as com10 until H tag available
              }
