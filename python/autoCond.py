d31067b96e723ce6c7bafc6d33a7dd900f1bd18a
Andreas Pfeiffer <sha1-c22fd1459612e19fa9e7e0f3174fc33733f2c2ad@cern.ch>
2010-11-23 18:35:37 +0000

    added starthi value for autoCond

--- python/autoCond.py
+++ python/autoCond.py
@@ -5,6 +5,7 @@ autoCond = { 'mc'      : 'MC_39Y_V6::All',
              'startup' : 'START39_V6::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
+             'starthi' : 'START39_V7HI::All',
              'com10'   : 'GR_R_39X_V3::All',
-             'hltonline' : 'GR10_H_V12::All' # same as com10 until H tag available
+             'hltonline' : 'GR10_H_V12::All', # same as com10 until H tag available
              }
