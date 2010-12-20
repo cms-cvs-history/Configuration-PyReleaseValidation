139c6b413a5443f455a7d9f796aba000f43024be
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2010-12-20 10:54:00 +0000

    Update GT for STARTUP with new Tk misalignment

--- python/autoCond.py
+++ python/autoCond.py
@@ -2,7 +2,7 @@
 
 
 autoCond = { 'mc'      : 'MC_39Y_V7::All',
-             'startup' : 'START39_V7::All',
+             'startup' : 'START39_V8::All',
              'craft08' : 'CRFT8_37R_V1::All',
              'craft09' : 'CRFT9_39R_V2::All',
              'starthi' : 'START39_V7HI::All',
