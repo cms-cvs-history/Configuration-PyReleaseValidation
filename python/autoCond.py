e416df4b00e2d845e71434efb2110e254fad16ac
cmsbuild <>
2011-03-08 11:12:33 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- PyReleaseValidation-CMSSW_4_2_0_pre7'.
    Sprout from master 2011-02-24 13:54:31 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'read in DQM file for harvesting step'
    Cherrypick from master 2011-03-08 11:12:32 UTC Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch> 'Update the GTs for 42X':
        python/autoCond.py

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,11 +1,11 @@
 
 
 
-autoCond = { 'mc'      : 'MC_42_V3::All',
-             'startup' : 'START42_V3::All',
+autoCond = { 'mc'      : 'MC_42_V6::All',
+             'startup' : 'START42_V6::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_42_V2::All',
-             'hltonline' : 'GR_R_42_V2::All', # same as com10 until H tag available
-             'starthi' : 'START311_V1HI::All'
+             'com10'   : 'GR_R_42_V6::All',
+             'hltonline' : 'GR_R_42_V6::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI42_V6::All'
              }
