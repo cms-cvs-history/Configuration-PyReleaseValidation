481f542e36334980dc3037717f2cd04c393a4b19
Gianluca Cerminara <sha1-e069a525311dcccf7987d451d144560439d7b950@cern.ch>
2011-05-05 09:28:46 +0000

    update GT for HLT for 42X

--- python/autoCond.py
+++ python/autoCond.py
@@ -6,6 +6,6 @@ autoCond = { 'mc'      : 'MC_42_V12::All',
              'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
              'com10'   : 'GR_R_42_V12::All',
-             'hltonline' : 'GR_R_42_V10::All', # same as com10 until H tag available
+             'hltonline' : 'GR_R_42_V12::All', # same as com10 until H tag available
              'starthi' : 'STARTHI42_V8::All'
              }
