ceedc5b508231e649449f1ea3aa5ae3cfc64131a
Andrea Bocci <sha1-f43ac2238d9995f8c883948414432a9e5f50335c@cern.ch>
2011-09-14 15:13:05 +0000

    revert to V14B for Fall11 production

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,5 +1,5 @@
-autoCond = { 'mc':          'MC_42_V15A::All',      # new L1 menu Collisions2011 v5 (3e33)
-             'startup':     'START42_V15A::All',    # new L1 menu Collisions2011 v5 (3e33)
+autoCond = { 'mc':          'MC_42_V14B::All',      # new L1 menu Collisions2011 v5 (3e33)
+             'startup':     'START42_V14B::All',    # new L1 menu Collisions2011 v5 (3e33)
              'craft08':     'CRFT8_37R_V1::All',    # not maintained anymore
              'craft09':     'CRFT9_310_V1::All',    # not maintained anymore
              'com10':       'GR_R_42_V14::All',
