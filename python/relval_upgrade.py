732d9b6f8e2cedb7a2bb266dd3fb890edf2dbf89
Gaelle Boudoul <sha1-a82f96c922d75aadc2b17eace2ae9543e16ce583@cern.ch>
2012-12-03 14:38:48 +0000

    reordering+ 8TeV workflows

--- python/relval_upgrade.py
+++ python/relval_upgrade.py
@@ -15,27 +15,38 @@ workflows = {}
 #workflows[3123] = ['', ['MinBias_UPGphase1_14']]
 #workflows[3135] = ['', ['TTbar_Tauola_UPGphase1_14']]
 
-#Postls1
-workflows[3223] = ['MinBias', ['MinBias_UPGpostls1_14','DIGIUP','RECOUP']]
-workflows[3235] = ['TTbar', ['TTbar_Tauola_UPGpostls1_14','DIGIUP','RECOUP']]
-
-workflows[3201] = ['JpsiMM', ['JpsiMM_UPGpostls1_14','DIGIUP','RECOUP']]
-workflows[3202] = ['WM', ['WM_UPGpostls1_14','DIGIUP','RECOUP']]
-workflows[3203] = ['ZMM', ['ZMM_UPGpostls1_14','DIGIUP','RECOUP']]
+#Postls1 
 workflows[3204] = ['SingleMuPt10', ['SingleMuPt10_UPGpostls1','DIGIUP','RECOUP']]
 workflows[3205] = ['SingleMuPt100', ['SingleMuPt100_UPGpostls1','DIGIUP','RECOUP']]
 workflows[3206] = ['SingleMuPt1000', ['SingleMuPt1000_UPGpostls1','DIGIUP','RECOUP']]
 
+
+#Postls1 14TeV
+
+workflows[3201] = ['JpsiMM', ['JpsiMM_UPGpostls1_14','DIGIUP','RECOUP']]
+workflows[3202] = ['WM', ['WM_UPGpostls1_14','DIGIUP','RECOUP']]
+workflows[3203] = ['ZMM', ['ZMM_UPGpostls1_14','DIGIUP','RECOUP']]
 workflows[3208] = ['ZmumuJets_Pt20_300', ['ZmumuJets_Pt20_300_UPGpostls1_14','DIGIUP','RECOUP']]
+workflows[3223] = ['MinBias', ['MinBias_UPGpostls1_14','DIGIUP','RECOUP']]
+workflows[3235] = ['TTbar', ['TTbar_Tauola_UPGpostls1_14','DIGIUP','RECOUP']]
 
-#postsl1 PU
-workflows[4235] = ['TTbar', ['TTbar_Tauola_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
-
-workflows[4201] = ['JpsiMM', ['JpsiMM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
-workflows[4202] = ['WM', ['WM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
-workflows[4203] = ['ZMM', ['ZMM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+#postsl1 PU50
 workflows[4204] = ['SingleMuPt10', ['SingleMuPt10_UPGpostls1','DIGIPUUP','RECOPUUP']]
 workflows[4205] = ['SingleMuPt100', ['SingleMuPt100_UPGpostls1','DIGIPUUP','RECOPUUP']]
 workflows[4206] = ['SingleMuPt1000', ['SingleMuPt1000_UPGpostls1','DIGIPUUP','RECOPUUP']]
 
+#postsl1 14TeV PU50
+workflows[4201] = ['JpsiMM', ['JpsiMM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+workflows[4202] = ['WM', ['WM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+workflows[4203] = ['ZMM', ['ZMM_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
 workflows[4208] = ['ZmumuJets_Pt20_300', ['ZmumuJets_Pt20_300_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+workflows[4223] = ['MinBias', ['MinBias_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+workflows[4235] = ['TTbar', ['TTbar_Tauola_UPGpostls1_14','DIGIPUUP','RECOPUUP']]
+
+#postsl1 PU 8TeV
+workflows[3301] = ['JpsiMM', ['JpsiMM_UPGpostls1_8','DIGIUP','RECOUP']]
+workflows[3302] = ['WM', ['WM_UPGpostls1_8','DIGIUP','RECOUP']]
+workflows[3303] = ['ZMM', ['ZMM_UPGpostls1_8','DIGIUP','RECOUP']]
+workflows[3308] = ['ZmumuJets_Pt20_300', ['ZmumuJets_Pt20_300_UPGpostls1_8','DIGIUP','RECOUP']]
+workflows[3323] = ['MinBias', ['MinBias_UPGpostls1_8','DIGIUP','RECOUP']]
+workflows[3335] = ['TTbar', ['TTbar_Tauola_UPGpostls1_8','DIGIUP','RECOUP']]
